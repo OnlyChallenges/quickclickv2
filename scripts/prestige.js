919a5b83e1a3b28cee1e882986eda5255d9d4f1d
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-09-06 14:43:32 -0400

    update stuff again because i want to.

--- scripts/prestige.js
+++ scripts/prestige.js
@@ -44,7 +44,7 @@ let Prestige = {
     } else {
       Runtime.updateRun(fastestrunholder, 'fastestrun')
     }
-    this.prestigePoints = prestigepointsholder + (1 * (1 + (Clicks.nbrOwned/1e11)))
+    this.prestigePoints += prestigepointsholder + 1
     this.updatePrestigePoints()
     this.updateBonus()
     this.prestigeShop()
